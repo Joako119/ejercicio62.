43e473a227d0846b66fdc93f886c3740033d36fe
Joako119 <joaquindeamelio400@gmail.com>
2023-07-08 20:04:23 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio62.numprimos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio62.numprimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, contador, primo;
+            bool salida;
+
+            salida = true;
+            contador = 0;
+            primo = 0;
+            do
+            {
+                Console.WriteLine("ingresar numero o si desea salir ingresar 0");
+                num = int.Parse(Console.ReadLine());
+
+
+
+                if (num == 0)
+
+                { salida = false; }
+                
+                else if (num % (2) == 1)
+                {
+                    Console.WriteLine("el numero es primo");
+                    primo = primo + 1;
+                }
+
+
+
+                else
+                    Console.WriteLine("el numero ingresado no es primo");
+            }
+
+
+            while (salida);
+            Console.WriteLine($"se ingresaron {primo} numeros primo");
+        }
+    }
+}
